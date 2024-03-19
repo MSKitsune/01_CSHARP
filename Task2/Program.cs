9d183fffaa0d38ff702701c9923d10cba02cc708
Sayara <sayarabirova@gmail.com>
2024-03-19 17:09:57 +0600

    lesson1:task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int produc1 = i1 * i2;
+long produc2 = l1 * i1;
+double produc3 = d1 *i1;
+Console.WriteLine(produc1);
+Console.WriteLine(produc2);
+Console.WriteLine(produc3);
