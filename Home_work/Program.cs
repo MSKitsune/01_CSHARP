8a6182e4ab398bd1f657e679f2bf1b32d428dadf
Sayara <sayarabirova@gmail.com>
2024-03-19 19:46:38 +0600

    add lesson1:HW

---
+++ Home_work/Program.cs
@@ -0,0 +1,13 @@
+﻿double a = 7;
+double b = 12;
+double c = 5;
+double d = 21;
+
+double product = a * b;
+// Console.WriteLine(product);
+
+double sum = c + d;
+// Console.WriteLine(sum);
+
+double quotient = product / sum;
+Console.WriteLine(quotient);
